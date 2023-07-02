35a725b8c6f2220879dc3b4f49d82f90c280364f
aansmirnov <aansmrnv@gmail.com>
2023-07-02 23:58:29 +0300

    feat(app): add eslint & dotenv

--- electron/index.js
+++ electron/index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+require('dotenv').config();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,7 @@
-export const App = () => <div className="bg-gray-900 h-screen">content</div>;
+export const App = () => {
+    return (
+        <div className="bg-gray-900 h-screen">
+            content
+        </div>
+    );
+};
