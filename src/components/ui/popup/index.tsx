2f0cfa40b44fe2e5eb1d2154a93f9201471af3f3
aansmirnov <aansmrnv@gmail.com>
2023-09-07 20:46:29 +0300

    chore(popup): update close icon color

--- src/components/ui/popup/index.tsx
+++ src/components/ui/popup/index.tsx
@@ -12,7 +12,7 @@ export const Popup = ({ children, onClose }: PopupProps) => (
         <div className="fixed inset-0 z-10 overflow-y-auto">
             <div id='overlay' className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                 <div className="relative transform rounded-[20px] bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                    <CloseIcon onClick={onClose} className='w-6 h-6 absolute right-[-30px] top-[-30px] hover:cursor-pointer' />
+                    <CloseIcon onClick={onClose} className='w-6 h-6 text-white absolute right-[-30px] top-[-30px] hover:cursor-pointer' />
                     <div className="bg-white px-7 py-9 rounded-[20px]">
                         {children}
                     </div>
