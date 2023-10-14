218e17b3c3235fc8ea44e1d32ecb5ae6c5a002e7
aansmirnov <aansmrnv@gmail.com>
2023-10-14 23:50:49 +0300

    feat(chat-store): save emotes and badges to loal storage

--- src/consts/local-storage.ts
+++ src/consts/local-storage.ts
@@ -1,2 +1,4 @@
 export const TWITCHER_CONFIG = 'twitcher_config';
 export const TWITCHER_ACCESS_TOKEN = 'twitcher_access_token';
+export const TWITCHER_EMOTES = 'twitcher_emotes';
+export const TWITCHER_BADGES = 'twitcher_badges';
--- src/stores/chat-store.ts
+++ src/stores/chat-store.ts
@@ -1,7 +1,18 @@
 import { action, makeObservable, observable, runInAction } from 'mobx';
 import { createContext, useContext } from 'react';
 import { apiHelix } from 'src/api';
+import { TWITCHER_BADGES, TWITCHER_EMOTES } from 'src/consts';
 import { Badges, BadgesMapBySetID, Emote, EmotesMapById } from 'src/types';
+
+const THREE_DAYS = 259200000;
+type LocalStorageData<T> = {
+    data: T;
+    expired_at: number;
+}
+type ChatData<T, U> = {
+    data: T;
+    map: U;
+}
 
 class ChatStore {
     badges: Badges[] = [];
@@ -20,7 +31,35 @@ class ChatStore {
         });
     }
 
+    private setChatDataToLocalStorage = <T>(key: string, data: T) => {
+        const object: LocalStorageData<T> = { data, expired_at: Date.now() + THREE_DAYS };
+        localStorage.setItem(key, JSON.stringify(object));
+    };
+
+    private getChatDataFromLocalStorage = <T>(key: string) => {
+        const dataFromLocalStorage = localStorage.getItem(key);
+
+        if (!dataFromLocalStorage) {
+            return null;
+        }
+
+        const parsedData = JSON.parse(dataFromLocalStorage) as unknown as LocalStorageData<T>;
+        if (Date.now() > parsedData.expired_at) {
+            return null;
+        }
+
+        return parsedData.data;
+    };
+
     getBadges = (userID: string) => {
+        const badges = this.getChatDataFromLocalStorage<ChatData<Badges[], BadgesMapBySetID>>(TWITCHER_BADGES);
+
+        if (badges) {
+            this.badges = badges.data;
+            this.badgesMapBySetID = badges.map;
+            return;
+        }
+
         Promise
             .all([apiHelix.getChanngelBadges({ broadcaster_id: userID }), apiHelix.getBadges()])
             .then((response) => {
@@ -32,10 +71,20 @@ class ChatStore {
                     this.badgesMapBySetID = allBadges
                         .reduce((acc, next) => ({ ...acc, [next.set_id]: { versions: next.versions[0], set_id: next.set_id } }), {});
                 });
+
+                this.setChatDataToLocalStorage(TWITCHER_BADGES, { data: this.badges, map: this.badgesMapBySetID });
             });
     };
 
     getEmotes = (userID: string) => {
+        const emotes = this.getChatDataFromLocalStorage<ChatData<Emote[], EmotesMapById>>(TWITCHER_EMOTES);
+
+        if (emotes) {
+            this.emotes = emotes.data;
+            this.emotesMapByID = emotes.map;
+            return;
+        }
+
         Promise
             .all([apiHelix.getChannelEmotes({ broadcaster_id: userID }), apiHelix.getEmotes()])
             .then((response) => {
@@ -47,6 +96,8 @@ class ChatStore {
                     this.emotesMapByID = allEmotes
                         .reduce((acc, next) => ({ ...acc, [next.id]: next }), {});
                 });
+
+                this.setChatDataToLocalStorage(TWITCHER_EMOTES, { data: this.emotes, map: this.emotesMapByID });
             });
     };
 }
